44d684fa58bfc2c09c8055b1f56928e809306956
Omar Lengua <omarns21@gmail.com>
2025-07-26 17:31:31 +0200

    feat: adding getters, setters and toString mthds.

--- src/main/java/com/aluracursos/screenmatch/model/Serie.java
+++ src/main/java/com/aluracursos/screenmatch/model/Serie.java
@@ -18,6 +18,76 @@ public class Serie {
         this.totalTemporadas = datosSerie.totalTemporadas();
         this.evaluacion = OptionalDouble.of(Double.valueOf(datosSerie.evaluacion())).orElse(0);
         this.poster = datosSerie.poster();
-        this.genero =
+        this.genero = Categoria.fromString(datosSerie.genero().split(",")[0].trim());
+        this.actores = datosSerie.actores();
+        this.sinopsis = datosSerie.sinopsis();
+    }
+
+    @Override
+    public String toString() {
+        return
+                "titulo='" + titulo + '\'' +
+                ", totalTemporadas=" + totalTemporadas +
+                ", evaluacion=" + evaluacion +
+                ", poster='" + poster + '\'' +
+                ", genero=" + genero +
+                ", actores='" + actores + '\'' +
+                ", sinopsis='" + sinopsis + '\'';
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public Integer getTotalTemporadas() {
+        return totalTemporadas;
+    }
+
+    public void setTotalTemporadas(Integer totalTemporadas) {
+        this.totalTemporadas = totalTemporadas;
+    }
+
+    public Double getEvaluacion() {
+        return evaluacion;
+    }
+
+    public void setEvaluacion(Double evaluacion) {
+        this.evaluacion = evaluacion;
+    }
+
+    public String getPoster() {
+        return poster;
+    }
+
+    public void setPoster(String poster) {
+        this.poster = poster;
+    }
+
+    public Categoria getGenero() {
+        return genero;
+    }
+
+    public void setGenero(Categoria genero) {
+        this.genero = genero;
+    }
+
+    public String getActores() {
+        return actores;
+    }
+
+    public void setActores(String actores) {
+        this.actores = actores;
+    }
+
+    public String getSinopsis() {
+        return sinopsis;
+    }
+
+    public void setSinopsis(String sinopsis) {
+        this.sinopsis = sinopsis;
     }
 }
