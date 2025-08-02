dab26a4f37cd2790334d5cd52297f4b5e107d3fc
Omar Lengua <omarns21@gmail.com>
2025-08-02 23:33:25 +0200

    feat: episodios table into db.

--- src/main/java/com/aluracursos/screenmatch/model/Serie.java
+++ src/main/java/com/aluracursos/screenmatch/model/Serie.java
@@ -12,6 +12,7 @@ public class Serie {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long Id;
+
     @Column(unique = true)
     private String titulo;
     private Integer totalTemporadas;
@@ -21,7 +22,7 @@ public class Serie {
     private Categoria genero;
     private String actores;
     private String sinopsis;
-    @Transient
+    @OneToMany(mappedBy = "serie")
     private List<Episodio> episodios;
 
     public Serie() {}
